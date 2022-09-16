531c061f86fd8e630ff5d352d1392d9b1f25d3a9
Alexey Reshetnyak <all.reshetnyak@gmail.com>
2022-09-16 20:03:46 +0300

    #84 - tx-history-for-fiat - edit history model by new history response (#107)

--- lib/models/fiat_history_model.dart
+++ lib/models/fiat_history_model.dart
@@ -2,69 +2,46 @@ import 'package:intl/intl.dart';
 
 class FiatHistoryModel {
   String? type;
-  double? buyAmount;
-  double? sellAmount;
-  double? fee;
-  String? buyAsset;
-  String? sellAsset;
-  String? feeAsset;
-  String? exchange;
-  String? tradeGroup;
-  String? comment;
+  double? inputAmount;
+  double? outputAmount;
+  String? inputAsset;
+  String? outputAsset;
   String? date;
   String? txid;
-  double? buyValueInEur;
-  double? sellValueInEur;
+  String? almCheck;
+  bool? isComplete;
 
   FiatHistoryModel({
     this.type,
-    this.buyAmount,
-    this.sellAmount,
-    this.fee,
-    this.buyAsset,
-    this.sellAsset,
-    this.feeAsset,
-    this.exchange,
-    this.tradeGroup,
-    this.comment,
+    this.inputAmount,
+    this.outputAmount,
+    this.inputAsset,
+    this.outputAsset,
     this.date,
     this.txid,
-    this.buyValueInEur,
-    this.sellValueInEur,
+    this.almCheck,
+    this.isComplete,
   });
 
   final DateFormat formatter = DateFormat('dd.MM.yyyy');
 
   FiatHistoryModel.fromJson(Map<String, dynamic> json) {
     this.type = json["type"];
-    if (json["buyAmount"] != null) {
-      this.buyAmount = double.parse(json["buyAmount"].toString());
+    if (json["inputAmount"] != null) {
+      this.inputAmount = double.parse(json["inputAmount"].toString());
     } else {
-      this.buyAmount = null;
+      this.inputAmount = null;
     }
-    if (json["sellAmount"] != null) {
-      this.sellAmount = double.parse(json["sellAmount"].toString());
+    if (json["outputAmount"] != null) {
+      this.outputAmount = double.parse(json["outputAmount"].toString());
     } else {
-      this.sellAmount = null;
+      this.outputAmount = null;
     }
-    if (json["fee"] != null) {
-      this.fee = double.parse(json["fee"].toString());
-    } else {
-      this.fee = null;
-    }
-    this.buyAsset = json["buyAsset"];
-    this.sellAsset = json["sellAsset"];
-    this.feeAsset = json["feeAsset"];
-    this.exchange = json["exchange"];
-    this.tradeGroup = json["tradeGroup"];
-    this.comment = json["comment"];
+    this.inputAsset = json["inputAsset"];
+    this.outputAsset = json["outputAsset"];
     this.date = formatter.format(DateTime.parse(json["date"]));
     this.txid = json["txid"];
-    if (json["buyValueInEur"] != null) {
-      this.buyValueInEur = double.parse(json["buyValueInEur"].toString());
-    }
-    if (json["sellValueInEur"] != null) {
-      this.sellValueInEur = double.parse(json["sellValueInEur"].toString());
-    }
+    this.almCheck = json["almCheck"];
+    this.isComplete =json["isComplete"];
   }
 }
