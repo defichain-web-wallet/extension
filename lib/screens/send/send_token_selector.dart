1c0d2c539c9767740bf0dc373cdaef62e00ebf0c
Alexey Reshetnyak <all.reshetnyak@gmail.com>
2022-09-19 14:38:11 +0300

    #91 - remove shadow - hot fix send token selector

--- lib/screens/send/send_token_selector.dart
+++ lib/screens/send/send_token_selector.dart
@@ -152,12 +152,9 @@ class _SendConfirmState extends State<SendTokenSelector> {
                     }
                   });
                 }
-                addressController.text = widget.selectedAddress;
 
                 return Container(
-                  color: isCustomBgColor
-                      ? Theme.of(context).dialogBackgroundColor
-                      : null,
+                  color: Theme.of(context).dialogBackgroundColor,
                   padding:
                       const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
                   child: Center(
@@ -167,62 +164,56 @@ class _SendConfirmState extends State<SendTokenSelector> {
                         children: [
                           Container(
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text('Address',
+                                Text('Recipient Address',
                                     style:
                                         Theme.of(context).textTheme.headline6),
-                                SizedBox(height: 16),
+                                SizedBox(height: 12),
                                 AddressField(
-                                  hideOverlay: hideOverlay,
                                   addressController: addressController,
-                                  onChanged: (value) {
-                                    if (isFailed) {
-                                      setState(() {
-                                        isFailed = false;
-                                      });
-                                    }
-                                  },
                                 ),
                                 SizedBox(height: 8),
-                                Text(
-                                  'Address is invalid',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline4!
-                                      .copyWith(
-                                        color: isFailedAddress
-                                            ? AppTheme.redErrorColor
-                                            : Colors.transparent,
-                                      ),
-                                ),
                                 Divider(),
                                 SizedBox(height: 12),
                                 Text(
-                                  'Amount',
+                                  'Asset',
                                   style: Theme.of(context).textTheme.headline6,
                                 ),
-                                SizedBox(height: 16),
+                                SizedBox(height: 12),
                                 AssetDropdown(
-                                  hideOverlay: hideOverlay,
                                   selectKeyFrom: _selectKeyFrom,
                                   amountController: _amountController,
                                   focusNode: _amountFocusNode,
                                   focusModel: _amountFocusModel,
                                   assets: assets,
                                   assetFrom: assetFrom,
+                                  amountInUsd: amountInUsd,
+                                  onChanged: (String value) {
+                                    if (SettingsHelper.isBitcoin()) {
+                                      try {
+                                        var amount = tokenHelper.getAmountByUsd(
+                                          tokensCubit.state.tokensPairs!,
+                                          double.parse(
+                                              value.replaceAll(',', '.')),
+                                          'BTC',
+                                        );
+                                        setState(() {
+                                          amountInUsd = balancesHelper
+                                              .numberStyling(amount,
+                                                  fixedCount: 2, fixed: true);
+                                        });
+                                      } catch (err) {
+                                        print(err);
+                                      }
+                                    }
+                                  },
                                   onSelect: (String asset) {
                                     setState(() => {assetFrom = asset});
                                   },
-                                  onChanged: (value) {
-                                    if (isFailed) {
-                                      setState(() {
-                                        isFailed = false;
-                                      });
-                                    }
-                                  },
                                   onPressedMax: () {
                                     setState(() {
-                                      hideOverlay();
                                       int balance = state
                                           .activeAccount!.balanceList!
                                           .firstWhere((el) =>
@@ -239,45 +230,85 @@ class _SendConfirmState extends State<SendTokenSelector> {
                                   isFixedWidthAssetSelectorText:
                                       isCustomBgColor,
                                 ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'Amount is invalid',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline4!
-                                      .copyWith(
-                                        color: isFailed
-                                            ? AppTheme.redErrorColor
-                                            : Colors.transparent,
-                                      ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 16.0),
-                                  child: Text(
-                                    'Insufficient funds',
+                                SizedBox(height: 12),
+                                if (SettingsHelper.isBitcoin())
+                                  Text(
+                                    "Available balance: ${_getAvailableBalance(bitcoinState)} BTC",
                                     style: Theme.of(context)
                                         .textTheme
                                         .headline4!
-                                        .copyWith(
-                                          color: _isBalanceError
-                                              ? AppTheme.redErrorColor
-                                              : Colors.transparent,
+                                        .apply(
+                                          color: Colors.grey,
                                         ),
                                   ),
-                                )
+                                SizedBox(height: 22),
+                                if (SettingsHelper.isBitcoin()) Text("Fees"),
+                                SizedBox(height: 12),
+                                if (bitcoinState.networkFee != null)
+                                  Column(
+                                    children: [
+                                      FeeCard(
+                                        fee: bitcoinState.networkFee!.low!,
+                                        iconUrl: 'assets/slow_$theme.svg',
+                                        label: 'Slow',
+                                        callback: () async {
+                                          bitcoinCubit.changeActiveFee(
+                                              state.activeAccount!
+                                                  .bitcoinAddress!,
+                                              bitcoinState.networkFee!.low!);
+                                        },
+                                        isActive: bitcoinState.activeFee ==
+                                            bitcoinState.networkFee!.low,
+                                      ),
+                                      SizedBox(height: 8),
+                                      FeeCard(
+                                        fee: bitcoinState.networkFee!.medium!,
+                                        iconUrl: 'assets/medium_$theme.svg',
+                                        label: 'Medium',
+                                        callback: () async {
+                                          bitcoinCubit.changeActiveFee(
+                                              state.activeAccount!
+                                                  .bitcoinAddress!,
+                                              bitcoinState.networkFee!.medium!);
+                                        },
+                                        isActive: bitcoinState.activeFee ==
+                                            bitcoinState.networkFee!.medium,
+                                      ),
+                                      SizedBox(height: 8),
+                                      FeeCard(
+                                        fee: bitcoinState.networkFee!.high!,
+                                        iconUrl: 'assets/fast_$theme.svg',
+                                        label: 'Fast',
+                                        callback: () async {
+                                          bitcoinCubit.changeActiveFee(
+                                              state.activeAccount!
+                                                  .bitcoinAddress!,
+                                              bitcoinState.networkFee!.high!);
+                                        },
+                                        isActive: bitcoinState.activeFee ==
+                                            bitcoinState.networkFee!.high,
+                                      )
+                                    ],
+                                  ),
                               ],
                             ),
                           ),
                           PrimaryButton(
                             label: 'Continue',
-                            callback: () {
-                              hideOverlay();
-                              submitToConfirm(state, transactionState);
-                            },
+                            callback: () => submitToConfirm(
+                                state, transactionState, bitcoinState),
                           ),
                         ],
                       ),
                     ),
+                  ),
+                );
+              } else if (bitcoinState.status == BitcoinStatusList.failure &&
+                  SettingsHelper.isBitcoin()) {
+                return Center(
+                  child: ErrorPlaceholder(
+                    message: 'Something went wrong',
+                    description: 'Please try later',
                   ),
                 );
               } else {
@@ -288,7 +319,7 @@ class _SendConfirmState extends State<SendTokenSelector> {
         },
       );
 
-  submitToConfirm(state, transactionState) async {
+  submitToConfirm(state, transactionState, bitcoinState) async {
     if (transactionState is TransactionLoadingState) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -301,13 +332,21 @@ class _SendConfirmState extends State<SendTokenSelector> {
       );
       return;
     }
-    var valid = await addressHelper.validateAddress(addressController.text);
-    if (valid) {
-      int balance = state.activeAccount.balanceList!
+    bool valid = false;
+    int balance = 0;
+    final int fee = 3000;
+    double amount = 0;
+    if (SettingsHelper.isBitcoin()) {
+      valid = await addressHelper.validateBtcAddress(addressController.text);
+      amount = convertFromSatoshi(bitcoinState.availableBalance);
+    } else {
+      valid = await addressHelper.validateAddress(addressController.text);
+      balance = state.activeAccount.balanceList!
           .firstWhere((el) => el.token! == assetFrom && !el.isHidden)
           .balance!;
-      final int fee = 3000;
-      double amount = convertFromSatoshi(balance - fee);
+      amount = convertFromSatoshi(balance - fee);
+    }
+    if (valid) {
       if (double.parse(_amountController.text.replaceAll(',', '.')) > 0) {
         if (double.parse(_amountController.text.replaceAll(',', '.')) <
             amount) {
@@ -320,31 +359,53 @@ class _SendConfirmState extends State<SendTokenSelector> {
                 addressController.text,
                 assetFrom,
                 double.parse(_amountController.text.replaceAll(',', '.')),
+                fee: bitcoinState.activeFee,
               ),
               transitionDuration: Duration.zero,
               reverseTransitionDuration: Duration.zero,
             ),
           );
         } else {
-          if (!_isBalanceError) {
-            setState(() {
-              _isBalanceError = true;
-            });
-          }
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Insufficient funds',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              backgroundColor: Theme.of(context).snackBarTheme.backgroundColor,
+            ),
+          );
         }
       } else {
-        if (!isFailed) {
-          setState(() {
-            isFailed = true;
-          });
-        }
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Invalid amount',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            backgroundColor: Theme.of(context).snackBarTheme.backgroundColor,
+          ),
+        );
       }
     } else {
-      if (!isFailedAddress) {
-        setState(() {
-          isFailedAddress = true;
-        });
-      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Invalid address',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          backgroundColor: Theme.of(context).snackBarTheme.backgroundColor,
+        ),
+      );
+    }
+  }
+
+  String _getAvailableBalance(state) {
+    double balance = convertFromSatoshi(state.availableBalance);
+    if (balance > 0) {
+      return balancesHelper.numberStyling(balance, fixed: true, fixedCount: 6);
+    } else {
+      return balancesHelper.numberStyling(0, fixed: true, fixedCount: 6);
     }
   }
 }
