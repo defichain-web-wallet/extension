f5b09a6fefff026b12a43507064e78564da17a9c
Alexey Reshetnyak <all.reshetnyak@gmail.com>
2022-10-24 17:24:58 +0300

    implement scaffold wrapper for all steps tutorial buy screen

--- lib/screens/buy/tutorials/first_step_buy_screen.dart
+++ lib/screens/buy/tutorials/first_step_buy_screen.dart
@@ -1,13 +1,12 @@
-import 'package:defi_wallet/bloc/account/account_cubit.dart';
+import 'package:defi_wallet/bloc/transaction/transaction_state.dart';
 import 'package:defi_wallet/config/config.dart';
 import 'package:defi_wallet/screens/buy/tutorials/second_step_buy_screen.dart';
 import 'package:defi_wallet/utils/app_theme/app_theme.dart';
 import 'package:defi_wallet/widgets/buttons/primary_button.dart';
 import 'package:defi_wallet/widgets/responsive/stretch_box.dart';
-import 'package:defi_wallet/widgets/scaffold_constrained_box.dart';
+import 'package:defi_wallet/widgets/scaffold_wrapper.dart';
 import 'package:defi_wallet/widgets/toolbar/main_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FirstStepBuyScreen extends StatefulWidget {
   const FirstStepBuyScreen({Key? key}) : super(key: key);
@@ -21,159 +20,147 @@ class _FirstStepBuyScreenState extends State<FirstStepBuyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AccountCubit, AccountState>(
-      builder: (BuildContext context, state) {
-        if (state.status == AccountStatusList.success) {
-          return ScaffoldConstrainedBox(
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth < ScreenSizes.medium) {
-                  return Scaffold(
-                    appBar: MainAppBar(
-                      title: 'How does it work?',
-                    ),
-                    body: _buildBody(state),
-                  );
-                } else {
-                  return Container(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Scaffold(
-                      appBar: MainAppBar(
-                        title: 'How does it work?',
-                        isSmall: true,
+    return ScaffoldWrapper(
+      builder: (
+        BuildContext context,
+        bool isFullScreen,
+        TransactionState txState,
+      ) {
+        return Scaffold(
+          appBar: MainAppBar(
+            title: 'How does it work?',
+            height: !(txState is TransactionInitialState)
+                ? ToolbarSizes.toolbarHeightWithBottom
+                : ToolbarSizes.toolbarHeight,
+            isSmall: isFullScreen,
+          ),
+          body: Container(
+            color: Theme.of(context).dialogBackgroundColor,
+            padding:
+                const EdgeInsets.only(left: 18, right: 12, top: 24, bottom: 24),
+            child: Center(
+              child: StretchBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: !isFullScreen ? 200 : 400,
+                              padding: EdgeInsets.only(
+                                top: 44,
+                              ),
+                              child: Image(
+                                image: AssetImage(
+                                    'assets/first_step_buy_logo.png'),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(
+                                top: 44,
+                              ),
+                              child: Text(
+                                '1. Select the asset you want.',
+                                // softWrap: true,
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline2
+                                    ?.apply(
+                                      fontSizeFactor: 1.6,
+                                    ),
+                              ),
+                            ),
+                            Container(
+                              width: 320,
+                              padding: EdgeInsets.only(
+                                top: 44,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      body: _buildBody(state, isFullSize: true),
                     ),
-                  );
-                }
-              },
+                    Container(
+                      width: 217,
+                      child: Column(
+                        children: [
+                          PrimaryButton(
+                            label: 'Next',
+                            isCheckLock: false,
+                            callback: () {
+                              Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                    pageBuilder:
+                                        (context, animation1, animation2) =>
+                                            SecondStepBuyScreen(
+                                      isConfirm: isConfirm,
+                                    ),
+                                    transitionDuration: Duration.zero,
+                                    reverseTransitionDuration: Duration.zero,
+                                  ));
+                            },
+                          ),
+                          Container(
+                            height: 48,
+                            padding: EdgeInsets.only(top: 10),
+                            child: StretchBox(
+                              child: Row(
+                                children: [
+                                  Checkbox(
+                                    value: isConfirm,
+                                    activeColor: AppTheme.pinkColor,
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        isConfirm = !isConfirm;
+                                      });
+                                    },
+                                  ),
+                                  Flexible(
+                                    child: MouseRegion(
+                                      cursor: SystemMouseCursors.click,
+                                      child: GestureDetector(
+                                        child: RichText(
+                                          text: TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text:
+                                                    'Don´t show this guide next time',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .subtitle1,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          setState(
+                                            () {
+                                              isConfirm = !isConfirm;
+                                            },
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          );
-        } else
-          return Container();
+          ),
+        );
       },
     );
   }
-
-  Widget _buildBody(state, {isFullSize = false}) => Container(
-        color: Theme.of(context).dialogBackgroundColor,
-        padding:
-            const EdgeInsets.only(left: 18, right: 12, top: 24, bottom: 24),
-        child: Center(
-          child: StretchBox(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: !isFullSize ? 200 : 400,
-                          padding: EdgeInsets.only(
-                            top: 44,
-                          ),
-                          child: Image(
-                            image: AssetImage('assets/first_step_buy_logo.png'),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.only(
-                            top: 44,
-                          ),
-                          child: Text(
-                            '1. Select the asset you want.',
-                            // softWrap: true,
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.headline2?.apply(
-                                  fontSizeFactor: 1.6,
-                                ),
-                          ),
-                        ),
-                        Container(
-                          width: 320,
-                          padding: EdgeInsets.only(
-                            top: 44,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 217,
-                  child: Column(
-                    children: [
-                      PrimaryButton(
-                        label: 'Next',
-                        isCheckLock: false,
-                        callback: () {
-                          Navigator.push(
-                              context,
-                              PageRouteBuilder(
-                                pageBuilder:
-                                    (context, animation1, animation2) =>
-                                        SecondStepBuyScreen(
-                                  isConfirm: isConfirm,
-                                ),
-                                transitionDuration: Duration.zero,
-                                reverseTransitionDuration: Duration.zero,
-                              ));
-                        },
-                      ),
-                      Container(
-                        height: 48,
-                        padding: EdgeInsets.only(top: 10),
-                        child: StretchBox(
-                          child: Row(
-                            children: [
-                              Checkbox(
-                                value: isConfirm,
-                                activeColor: AppTheme.pinkColor,
-                                onChanged: (newValue) {
-                                  setState(() {
-                                    isConfirm = !isConfirm;
-                                  });
-                                },
-                              ),
-                              Flexible(
-                                child: MouseRegion(
-                                  cursor: SystemMouseCursors.click,
-                                  child: GestureDetector(
-                                    child: RichText(
-                                      text: TextSpan(
-                                        children: [
-                                          TextSpan(
-                                            text:
-                                                'Don´t show this guide next time',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subtitle1,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    onTap: () {
-                                      setState(
-                                        () {
-                                          isConfirm = !isConfirm;
-                                        },
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
 }
--- lib/screens/buy/tutorials/third_step_buy_screen.dart
+++ lib/screens/buy/tutorials/third_step_buy_screen.dart
@@ -1,5 +1,6 @@
 import 'package:defi_wallet/bloc/account/account_cubit.dart';
 import 'package:defi_wallet/bloc/fiat/fiat_cubit.dart';
+import 'package:defi_wallet/bloc/transaction/transaction_state.dart';
 import 'package:defi_wallet/config/config.dart';
 import 'package:defi_wallet/screens/buy/contact_screen.dart';
 import 'package:defi_wallet/screens/buy/search_buy_token.dart';
@@ -7,6 +8,7 @@ import 'package:defi_wallet/utils/app_theme/app_theme.dart';
 import 'package:defi_wallet/widgets/buttons/primary_button.dart';
 import 'package:defi_wallet/widgets/responsive/stretch_box.dart';
 import 'package:defi_wallet/widgets/scaffold_constrained_box.dart';
+import 'package:defi_wallet/widgets/scaffold_wrapper.dart';
 import 'package:defi_wallet/widgets/toolbar/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -26,161 +28,151 @@ class ThirdStepBuyScreen extends StatefulWidget {
 class _ThirdStepBuyScreenState extends State<ThirdStepBuyScreen> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AccountCubit, AccountState>(
-      builder: (BuildContext context, state) {
-        if (state.status == AccountStatusList.success) {
-          return ScaffoldConstrainedBox(
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth < ScreenSizes.medium) {
-                  return Scaffold(
-                    appBar: MainAppBar(
-                      title: 'How does it work?',
-                    ),
-                    body: _buildBody(state),
-                  );
-                } else {
-                  return Container(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Scaffold(
-                      appBar: MainAppBar(
-                        title: 'How does it work?',
-                        isSmall: true,
+    return ScaffoldWrapper(
+      builder: (
+        BuildContext context,
+        bool isFullScreen,
+        TransactionState txState,
+      ) {
+        return Scaffold(
+          appBar: MainAppBar(
+            title: 'How does it work?',
+            height: !(txState is TransactionInitialState)
+                ? ToolbarSizes.toolbarHeightWithBottom
+                : ToolbarSizes.toolbarHeight,
+            isSmall: isFullScreen,
+          ),
+          body: Container(
+            color: Theme.of(context).dialogBackgroundColor,
+            padding:
+                const EdgeInsets.only(left: 18, right: 12, top: 24, bottom: 24),
+            child: Center(
+              child: StretchBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: !isFullScreen ? 200 : 400,
+                              padding: EdgeInsets.only(
+                                top: 44,
+                              ),
+                              child: Image(
+                                image: AssetImage(
+                                    'assets/third_step_buy_logo.png'),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(
+                                top: 44,
+                              ),
+                              child: Text(
+                                '3. We will transfer your cryptocurrencies directly into your wallet',
+                                softWrap: true,
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline2
+                                    ?.apply(
+                                      fontSizeFactor: 1.6,
+                                    ),
+                              ),
+                            ),
+                            Container(
+                              width: 320,
+                              padding: EdgeInsets.only(
+                                top: 44,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      body: _buildBody(state, isFullSize: true),
                     ),
-                  );
-                }
-              },
+                    Container(
+                      width: 217,
+                      child: Column(
+                        children: [
+                          PrimaryButton(
+                            label: 'Next',
+                            isCheckLock: false,
+                            callback: () async {
+                              FiatCubit fiatCubit =
+                                  BlocProvider.of<FiatCubit>(context);
+                              await fiatCubit
+                                  .changeStatusTutorial(!widget.isConfirm);
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder:
+                                      (context, animation1, animation2) =>
+                                          ContactScreen(),
+                                  transitionDuration: Duration.zero,
+                                  reverseTransitionDuration: Duration.zero,
+                                ),
+                              );
+                            },
+                          ),
+                          Container(
+                            height: 48,
+                            padding: EdgeInsets.only(top: 10),
+                            child: StretchBox(
+                              child: Row(
+                                children: [
+                                  Checkbox(
+                                    value: widget.isConfirm,
+                                    activeColor: AppTheme.pinkColor,
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        widget.isConfirm = !widget.isConfirm;
+                                      });
+                                    },
+                                  ),
+                                  Flexible(
+                                    child: MouseRegion(
+                                      cursor: SystemMouseCursors.click,
+                                      child: GestureDetector(
+                                        child: RichText(
+                                          text: TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text:
+                                                    'Don´t show this guide next time',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .subtitle1,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          setState(
+                                            () {
+                                              widget.isConfirm =
+                                                  !widget.isConfirm;
+                                            },
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          );
-        } else
-          return Container();
+          ),
+        );
       },
     );
   }
-
-  Widget _buildBody(state, {isFullSize = false}) => Container(
-        color: Theme.of(context).dialogBackgroundColor,
-        padding:
-            const EdgeInsets.only(left: 18, right: 12, top: 24, bottom: 24),
-        child: Center(
-          child: StretchBox(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: !isFullSize ? 200 : 400,
-                          padding: EdgeInsets.only(
-                            top: 44,
-                          ),
-                          child: Image(
-                            image: AssetImage('assets/third_step_buy_logo.png'),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.only(
-                            top: 44,
-                          ),
-                          child: Text(
-                            '3. We will transfer your cryptocurrencies directly into your wallet',
-                            softWrap: true,
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.headline2?.apply(
-                                  fontSizeFactor: 1.6,
-                                ),
-                          ),
-                        ),
-                        Container(
-                          width: 320,
-                          padding: EdgeInsets.only(
-                            top: 44,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 217,
-                  child: Column(
-                    children: [
-                      PrimaryButton(
-                        label: 'Next',
-                        isCheckLock: false,
-                        callback: () async {
-                          FiatCubit fiatCubit =
-                              BlocProvider.of<FiatCubit>(context);
-                          await fiatCubit
-                              .changeStatusTutorial(!widget.isConfirm);
-                          Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                              pageBuilder: (context, animation1, animation2) =>
-                                  ContactScreen(),
-                              transitionDuration: Duration.zero,
-                              reverseTransitionDuration: Duration.zero,
-                            ),
-                          );
-                        },
-                      ),
-                      Container(
-                        height: 48,
-                        padding: EdgeInsets.only(top: 10),
-                        child: StretchBox(
-                          child: Row(
-                            children: [
-                              Checkbox(
-                                value: widget.isConfirm,
-                                activeColor: AppTheme.pinkColor,
-                                onChanged: (newValue) {
-                                  setState(() {
-                                    widget.isConfirm = !widget.isConfirm;
-                                  });
-                                },
-                              ),
-                              Flexible(
-                                child: MouseRegion(
-                                  cursor: SystemMouseCursors.click,
-                                  child: GestureDetector(
-                                    child: RichText(
-                                      text: TextSpan(
-                                        children: [
-                                          TextSpan(
-                                            text:
-                                                'Don´t show this guide next time',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subtitle1,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    onTap: () {
-                                      setState(
-                                        () {
-                                          widget.isConfirm = !widget.isConfirm;
-                                        },
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
 }
