ced298121cd4ee2330ff165d44adeaa13c1d2c42
Alexey Reshetnyak <alexeyreshetnyak@Alexeys-MacBook-Pro.local>
2022-12-08 00:52:18 +0200

    fix draggble long press

--- lib/test_draggable.dart
+++ lib/test_draggable.dart
@@ -19,48 +19,22 @@ class _TestDraggableState extends State<TestDraggable> {
   @override
   void initState() {
     super.initState();
-    // listTest = <String>[
-    //   'Container',
-    //   'filter_2',
-    //   'filter_3',
-    //   'filter_4',
-    //   'filter_5',
-    //   'filter_6',
-    //   'filter_7',
-    //   'filter_8',
-    //   'filter_9',
-    // ];
-    // _tiles = <Widget>[
-    //   Container(
-    //     width: 40,
-    //     height: 40,
-    //     color: Colors.amber,
-    //   ),
-    //   Icon(Icons.filter_2, size: _iconSize),
-    //   Icon(Icons.filter_3, size: _iconSize),
-    //   Icon(Icons.filter_4, size: _iconSize),
-    //   Icon(Icons.filter_5, size: _iconSize),
-    //   Icon(Icons.filter_6, size: _iconSize),
-    //   Icon(Icons.filter_7, size: _iconSize),
-    //   Icon(Icons.filter_8, size: _iconSize),
-    //   Icon(Icons.filter_9, size: _iconSize),
-    // ];
+  }
+
+  void _onReorder(int oldIndex, int newIndex) {
+    String word = listTest.removeAt(oldIndex);
+    listTest.insert(newIndex, word);
+    setState(() {
+      print('oldIndex $oldIndex');
+      print('newIndex $newIndex');
+
+      Widget row = _tiles.removeAt(oldIndex);
+      _tiles.insert(newIndex, row);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    void _onReorder(int oldIndex, int newIndex) {
-      String word = listTest.removeAt(oldIndex);
-      listTest.insert(newIndex, word);
-      setState(() {
-        print('oldIndex $oldIndex');
-        print('newIndex $newIndex');
-
-        Widget row = _tiles.removeAt(oldIndex);
-        _tiles.insert(newIndex, row);
-      });
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('TestDraggable!'),
@@ -68,51 +42,30 @@ class _TestDraggableState extends State<TestDraggable> {
       body: Column(
         children: [
           Container(
+            width: 400,
+            color: Colors.grey.withOpacity(0.5),
             child: _tiles.isNotEmpty
-                ? ReorderableWrap(
-                    spacing: 8.0,
-                    runSpacing: 4.0,
-                    padding: const EdgeInsets.all(8),
-                    children: _tiles,
-                    onReorder: _onReorder,
-                    onNoReorder: (int index) {
-                      //this callback is optional
-                      debugPrint(
-                          '${DateTime.now().toString().substring(5, 22)} reorder cancelled. index:$index');
-                    },
-                    onReorderStarted: (int index) {
-                      //this callback is optional
-                      debugPrint(
-                          '${DateTime.now().toString().substring(5, 22)} reorder started: index:$index');
-                    },
-                  )
-                : Center(child: Text('Empty')),
-          ),
-          Container(
-            child: Center(
-              child: _tiles.isNotEmpty
-                  ? _tiles[0]
-                  : Center(
-                      child: Text('Empty'),
-                    ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              child: listTest.isNotEmpty
-                  ? ListView.builder(
-                      controller: ScrollController(),
-                      itemCount: listTest.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          child: Text(listTest[index]),
-                        );
+                ? Center(
+                  child: ReorderableWrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      needsLongPressDraggable: false,
+                      padding: const EdgeInsets.all(8),
+                      children: _tiles,
+                      onReorder: _onReorder,
+                      onNoReorder: (int index) {
+                        //this callback is optional
+                        debugPrint(
+                            '${DateTime.now().toString().substring(5, 22)} reorder cancelled. index:$index');
                       },
-                    )
-                  : Center(
-                      child: Text('Empty'),
+                      onReorderStarted: (int index) {
+                        //this callback is optional
+                        debugPrint(
+                            '${DateTime.now().toString().substring(5, 22)} reorder started: index:$index');
+                      },
                     ),
-            ),
+                )
+                : Center(child: Text('Empty')),
           ),
           TextField(
             decoration: InputDecoration(),
---
+++ lib/widgets/auth/mnemonic_word.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MnemonicWorld extends StatelessWidget {
+  const MnemonicWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+
+      ),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(),
+            TextSpan(),
+          ]
+        ),
+      ),
+    );
+  }
+}
