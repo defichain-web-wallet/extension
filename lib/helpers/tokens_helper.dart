9702097b63b21686cfeb2c21aa0e9df233d8df3e
Alexey Reshetnyak <all.reshetnyak@gmail.com>
2023-03-21 15:02:51 +0200

    #286 - add-token-description - adden desctiption for noname tokens

--- lib/helpers/tokens_helper.dart
+++ lib/helpers/tokens_helper.dart
@@ -12,6 +12,64 @@ class TokensHelper {
   static const String DefiTokenSymbol = '\$DFI';
   final _random = math.Random();
 
+  static const Map<String, String> noNamedTokens = {
+    'dAAPL': 'Apple',
+    'dAMZN': 'Amazon',
+    'dBABA': 'Alibaba',
+    'dCOIN': 'Coinbase',
+    'dGME': 'Gamestop',
+    'dGOOGL': 'Google',
+    'dNVDA': 'Nvidia',
+    'dPLTR': 'Palantir',
+    'dTSLA': 'Tesla',
+    'dARKK': 'New Tech.',
+    'dGLD': 'Gold',
+    'dSLV': 'Silver',
+    'dPDBC': 'Commodities',
+    'dQQQ': 'Nasdaq',
+    'dSPY': 'S&P',
+    'dTLT': 'US Treas. 20Y',
+    'dURTH': 'MSCI World',
+    'dVNQ': 'Real Estate',
+    'dEEM': 'Emerg.Markets',
+    'dFB': 'Facebook',
+    'dMSFT': 'Microsoft',
+    'dNFLX': 'Netflix',
+    'dVOO': 'S&P ETF',
+    'dDIS': 'Disney',
+    'dMSTR': 'Microstrat.',
+    'dMCHI': 'MSCI China',
+    'dINTC': 'Intel',
+    'dBRK.B': 'Berkshire',
+    'dPYPL': 'Paypal',
+    'dPG': 'Procter&Gm.',
+    'dKO': 'CocaCola',
+    'dCS': 'Credit Suisse',
+    'dSAP': 'SAP SE',
+    'dGSG': 'Commod. ETF',
+    'dURA': 'Uran ETF',
+    'dXOM': 'Exxon',
+    'dGOVT': 'US Treas. ETF',
+    'dTAN': 'Solar ETF',
+    'dPPLT': 'Platinum ETF',
+    'dJNJ': 'Johnson & J.',
+    'dDAX': 'Dax ETF',
+    'dADDYY': 'Adidas',
+    'dGS': 'Goldm. Sachs',
+    'dUNG': 'US Gas Fund',
+    'dUSO': 'US Oil Fund',
+    'dWMT': 'Walmart',
+    'dUL': 'Unilever',
+    'dARKX': 'Space ETF',
+    'dVBK': 'Sm.Caps ETF',
+    'dXLRE': 'Real Est. ETF',
+    'dXLE': 'Energy Fund',
+    'dNSRGY': 'Nestle',
+    'dSH': 'Short S&P ETF',
+    'dSHEL': 'Shell',
+    'dBITI': 'BTC Inv ETF',
+  };
+
   double getMaxAPR(List<AssetPairModel> tokenPairs) {
     double maxValue = 0;
 
@@ -1219,6 +1277,8 @@ class TokensHelper {
         return 'DeFiChain';
       } else if (value == defaultBitcoinTokenName) {
         return 'DeFiChain Bitcoin';
+      } else if(noNamedTokens.containsKey(value)){
+        return noNamedTokens[value]!;
       } else {
         return value;
       }
