48629bbfc3cbbf953f552b51747bdd9f3db9ff01
Alexey Reshetnyak <all.reshetnyak@gmail.com>
2023-04-03 20:40:57 +0300

    #388 - fix-ibans-for-buy - fixed ibans for buy (#389)

--- lib/bloc/fiat/fiat_cubit.dart
+++ lib/bloc/fiat/fiat_cubit.dart
@@ -178,7 +178,7 @@ class FiatCubit extends Cubit<FiatState> {
     List<IbanModel> activeIbanList = ibanList
         .where((el) =>
             el.active! &&
-            el.type == 'Wallet' &&
+            el.type == null &&
             asset!.name == el.asset!.name)
         .toList();
     IbanModel? iban;
