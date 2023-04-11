5f50cb1100eb61e6dc129e27fa612aacd434d761
Alexey Reshetnyak <all.reshetnyak@gmail.com>
2023-04-11 13:49:52 +0300

    #399 - fix-overflow-staking-kyc - fixed overflow APY amount for start… (#400)
    * #399 - fix-overflow-staking-kyc - fixed overflow APY amount for starting screen Staking KYC
    
    * #399 - fix-overflow-staking-kyc - refactoring
    
    * #399 - fix-overflow-staking-kyc - refactoring

--- lib/screens/staking/kyc/staking_kyc_start_screen.dart
+++ lib/screens/staking/kyc/staking_kyc_start_screen.dart
@@ -109,20 +109,8 @@ class _StakingKycStartScreenState extends State<StakingKycStartScreen>
                                           height: 4,
                                         ),
                                         Text(
-                                          '${BalancesHelper().numberStyling(
-                                            (lockState.lockAnalyticsDetails!
-                                                    .apy! *
-                                                100),
-                                            fixed: true,
-                                            fixedCount: 2,
-                                          )}% '
-                                          'APY / ${BalancesHelper().numberStyling(
-                                            (lockState.lockAnalyticsDetails!
-                                                    .apr! *
-                                                100),
-                                            fixed: true,
-                                            fixedCount: 2,
-                                          )}% APR',
+                                          '${getAprOrApyFormat(lockState.lockAnalyticsDetails!.apy!, 'APY')} / '
+                                          '${getAprOrApyFormat(lockState.lockAnalyticsDetails!.apr!, 'APR')}',
                                           style: Theme.of(context)
                                               .textTheme
                                               .headline5!
@@ -178,7 +166,7 @@ class _StakingKycStartScreenState extends State<StakingKycStartScreen>
                                                   ),
                                                   Text(
                                                     'Stake DFI and earn up to '
-                                                    '${lockState.lockAnalyticsDetails!.apy! * 100}% APY',
+                                                    '${getAprOrApyFormat(lockState.lockAnalyticsDetails!.apy!, 'APY')}',
                                                     style: Theme.of(context)
                                                         .textTheme
                                                         .subtitle1!
@@ -364,5 +352,13 @@ class _StakingKycStartScreenState extends State<StakingKycStartScreen>
         );
       },
     );
+  }
+  String getAprOrApyFormat(double amount, String amountType){
+    return '${BalancesHelper().numberStyling(
+      (amount *
+          100),
+      fixed: true,
+      fixedCount: 2,
+    )}% $amountType';
   }
 }
