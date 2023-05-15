81a735f267594a004f338ce2aa001044756ec8b0
Alex Resh <all.reshetntyak@gmail.com>
2023-05-15 16:42:30 +0300

    refactoring-network - update hive service

--- lib/services/storage/hive_service.dart
+++ lib/services/storage/hive_service.dart
@@ -1,11 +1,7 @@
-import 'package:bip32_defichain/bip32.dart' as bip32;
 import 'package:defi_wallet/client/hive_names.dart';
 import 'package:defi_wallet/helpers/encrypt_helper.dart';
 import 'package:defi_wallet/models/network/network_name.dart';
-import 'package:defichaindart/defichaindart.dart';
-import 'package:defi_wallet/helpers/network_helper.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:defichaindart/src/models/networks.dart' as networks;
 
 class HiveService {
   static Future<String> getMasterKey(
@@ -21,5 +17,42 @@ class HiveService {
     var box = await Hive.openBox(HiveBoxes.client);
     var encryptedMasterKey = await box.get(boxKey);
     return EncryptHelper.getDecryptedData(encryptedMasterKey, password);
+  }
+
+  static Future<dynamic> getData(
+    NetworkTypeModel network,
+    String key, {
+    String password = '',
+  }) async {
+    try {
+      var box = await Hive.openBox(HiveBoxes.client);
+      var data = await box.get(key);
+
+      return password.isNotEmpty
+          ? EncryptHelper.getDecryptedData(data, password)
+          : data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static Future<void> update(
+    NetworkTypeModel network,
+    String key,
+    dynamic data, {
+    String password = '',
+  }) async {
+    try {
+      var box = await Hive.openBox(HiveBoxes.client);
+
+      if (password.isNotEmpty) {
+        var encryptedData = EncryptHelper.getEncryptedData(data, password);
+        await box.put(key, encryptedData);
+      } else {
+        await box.put(key, data);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
